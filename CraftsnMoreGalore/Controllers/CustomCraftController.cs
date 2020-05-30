474dbc42d192824dff696ab15501b8d189293a36
Ryan Bressette <bressetteryan@gmail.com>
2020-05-29 21:10:36 -0400

    Add controller and views for creating custom crafts

---
+++ CraftsnMoreGalore/Controllers/CustomCraftController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CraftsnMoreGalore.Controllers
+{
+    public class CustomCraftController : Controller
+    {
+        public ActionResult Wreaths()
+        {
+            return View();
+        }
+
+        public ActionResult Shirts()
+        {
+            return View();
+        }
+    }
+}
